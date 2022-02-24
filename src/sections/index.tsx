db45e725de16aa1898241fce0792bc1fc569fbad
Romahlio Williams <romahlio.williams@hellofresh.ca>
2022-02-23 19:53:26 -0500

    team section done

--- src/sections/index.tsx
+++ src/sections/index.tsx
@@ -4,3 +4,4 @@ export { default as Hero } from './hero/Hero';
 export { default as About } from './about/About';
 export { default as Roadmap } from './roadmap/Roadmap';
 export { default as Utility } from './utility/Utility';
+export { default as Team } from './team/Team';
